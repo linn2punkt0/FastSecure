7bc0a2cdd4397a5a609bdf1ab4cae11b1d805167
Erik Arvidsson <e@Es-MacBook-Pro.local>
2018-11-07 13:35:24 +0100

    added data.php
    added svContent array and enContent array

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$svContent = [
+[  'info' => 'lalala',
+  'test' => 'hahaha'],
+[  'info' => 'lalala',
+  'test' => 'hahaha']
+];
+
+
+
+$enContent = [
+  ['title' => 'lalala',
+  'content' => 'hahaha'],
+  ['info2' => 'lalala',
+  'test2' => 'hahaha']
+];
