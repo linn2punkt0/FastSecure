9b1deb153362dad78439d3c6079d480fad1d4843
Erik Arvidsson <e@Es-MacBook-Pro.local>
2018-11-09 10:30:25 +0100

    updated

---
+++ counter.js
@@ -0,0 +1,14 @@
+var countDown = new Date('june 21 2018 15:00:00').getTime();
+
+var x = setInterval(() =>{
+
+  var today = new Date().getTime();
+
+  var distance = countDown - today;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var secounds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("time").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+}, 1000 );
--- index.php
+++ index.php
@@ -4,6 +4,11 @@ require __DIR__."/header.php";
 
   <h2><?= $content['headerContent']['title'] ?></h2>
 
+  <div class="content">
+    <p id="time"></p>
+  </div>
+    <script type="text/javascript" src="counter.js"></script>
+
 <?php
 require __DIR__."/signup.php";
 require __DIR__."/footer.php";
