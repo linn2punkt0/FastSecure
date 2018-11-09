985af8f0de687cbef272a0c4063757e7d6c3e7f7
Isabelle Krall <isabellekrall@Isabelles-MacBook-Pro.local>
2018-11-09 11:11:25 +0100

    tested fonts

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require __DIR__."/header.php";
   </div>
     <script type="text/javascript" src="counter.js"></script>
 
+<h2 class="test">Detta är test</h2>
+
 <?php
 require __DIR__."/signup.php";
 require __DIR__."/footer.php";
