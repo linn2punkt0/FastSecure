15277da6c0e3cd7fb387cef59d6bfe6b06a86c5c
Isabelle Krall <isabellekrall@gmail.com>
2018-11-14 15:09:00 +0100

    deleted test

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 require __DIR__."/header.php";
 ?>
 
-  <h2><?= $content['headerContent']['title'] ?></h2>
 
-  <div class="content">
-    <p id="time"></p>
-  </div>
-    <script type="text/javascript" src="counter.js"></script>
-
-<h2 class="test">Detta är test</h2>
+<div class="button2">
+  Headline
+</div>
 
 <?php
 require __DIR__."/signup.php";
