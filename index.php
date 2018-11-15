ad7cb95c16eb35a042d5bb80e51572ea7c29530d
Isabelle Krall <isabellekrall@gmail.com>
2018-11-15 12:56:27 +0100

    added classes

--- index.php
+++ index.php
@@ -3,11 +3,33 @@ require __DIR__."/header.php";
 ?>
 
 
+<div class="first-page"></div>
+<div class="scroll-down">Scroll down</div>
 
-<h2 class="test">Detta är test</h2>
+<div class="second-page">
+  <div class="headline">
+    <?=$content['headerContent']['title']?>
+  </div>
+</div>
+
+<div class="third-page">
+  <div class="headline">
+    <?=$content['secondContent']['title']?>
+  </div>
+</div>
+<div class="fourth-page">
+  <div class="headline">
+    <?=$content['FS1.0']['title']?>
+  </div>
+  <div class="content-box">
+    <?=$content['FS1.0']['content']?>
+  </div>
+  <div class="newsletter-button">
+    <?=$content['FS1.0']['button']?>
+  </div>
 
 
 <?php
-require __DIR__."/signup.php";
+//require __DIR__."/signup.php";
 require __DIR__."/footer.php";
 ?>
