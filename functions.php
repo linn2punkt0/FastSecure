1768e1fd242223b1757cacdd4274298aea78ecdb
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-11-16 14:55:50 +0100

    MAJOR BREAKTHROUGH

--- functions.php
+++ functions.php
@@ -1,14 +1,21 @@
 <?php
 declare(strict_types=1);
 
-
 function trans($key1, $key2) {
-  $lang = $_GET['lang'];
-  $language= $lang;
-
-  $locale = $language ?? 'en';
+  if(isset($_GET['lang'])){
+    $lang = $_GET['lang'];
+  }
+  else{
+    $lang = $_COOKIE['lang'];
+  }
+  $locale = $lang ?? 'en';
 
   $translations = require __DIR__.'/lang/'.$locale.'.php';
 
   return $Content[$key1][$key2];
 };
+
+function set_url( $url )
+{
+    echo("<script>history.replaceState({},'','$url');</script>");
+}
--- index.php
+++ index.php
@@ -3,41 +3,54 @@
 require __DIR__."/header.php";
 require __DIR__."/functions.php";
 
+$page = 'index';
 
+
+
+// THIS IS THE LANGUANGE SELECTOR
 if (isset($_GET['lang'])){
   $lang = $_GET['lang'];
   setcookie('lang', $lang);
+  set_url("index.php?lang=$lang&page=$page");
 }
-elseif (!isset($_GET['lang'])){
-  header("Location: index.php?lang=en");
+elseif (isset($_COOKIE['lang'])) {
+    $lang = $_COOKIE['lang'];
+    set_url("index.php?lang=$lang&page=$page");
 }
-
-
+elseif ((!isset($_GET['lang'])) || (!isset($_COOKIE['lang']))){
+    $lang= 'en';
+    setcookie('lang', $lang);
+    set_url("index.php?lang=$lang&page=$page");
+  }
 //   header("Location: index.php");
 
+
+
+// THIS IS THA PAGE SELECTOR
 if (isset($_GET['page'])) {
     $page = $_GET['page'] ?? 'index';
 
     if ($page === 'menu') {
         require __DIR__."/menu.php";
-
+        set_url("index.php?lang=$lang&page=$page");
     }
-    elseif ($page === 'index') {
+    if ($page === 'index') {
         require __DIR__."/navbar.php";
         require __DIR__."/main-content.php";
         require __DIR__."/signup.php";
 
         require __DIR__."/footer.php";
-
+        set_url("index.php?lang=$lang&page=$page");
     }
   }
   else {
     require __DIR__."/navbar.php";
     require __DIR__."/main-content.php";
     require __DIR__."/signup.php";
-
     require __DIR__."/footer.php";
+    set_url("index.php?lang=$lang&page=$page");
   }
+  
 
 
 
