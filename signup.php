de9dd69b0292b4ba9ee166c540313fcc635dbcac
Erik Arvidsson <e@bs61i-eb54ce5f9.staden.gotheborg.net>
2018-11-16 10:16:49 +0100

    added info function to signup

--- signup.php
+++ signup.php
@@ -14,7 +14,7 @@ if(isset($_POST['email'], $_POST['firstname'], $_POST['lastname'])) {
   }
 };
 ?>
-        <main class="container">
+        <main class="container" id="signup">
             <form class="col-lg-6 mt-3" action="signup.php" method="post">
 
               <?php foreach ($errors as $error): ?>
@@ -24,19 +24,19 @@ if(isset($_POST['email'], $_POST['firstname'], $_POST['lastname'])) {
               <?php endforeach; ?>
 
                 <div class="form-group">
-                    <label for="firstname"><?= $_SESSION['content']['contactForm']['firstName']?></label>
+                    <label for="firstname"><?=trans('contactForm','firstName')?></label>
                     <input type="text" name="firstname" id="text" class="form-control">
                 </div><!-- /form-group -->
 
                 <div class="form-group">
-                    <label for="firstname"><?= $_SESSION['content']['contactForm']['lastName']?></label>
+                    <label for="firstname"><?=trans('contactForm','lastName')?></label>
                     <input type="text" name="lastname" id="text" class="form-control">
                 </div><!-- /form-group -->
 
                 <div class="form-group">
-                    <label for="email"><?= $_SESSION['content']['contactForm']['email']?></label>
+                    <label for="email"><?=trans('contactForm','email')?></label>
                     <input type="email" name="email" id="email" class="form-control" placeholder="email@email.com">
                 </div><!-- /form-group -->
-                <button type="submit" class="btn btn-primary"><?= $_SESSION['content']['contactForm']['button']?></button>
+                <button type="submit" class="btn btn-primary"><?=trans('contactForm','button')?></button>
             </form>
         </main><!-- /container -->
