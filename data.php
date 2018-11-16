80c117cb4796cbc52eee1eedb4fa6667637f4d3e
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-11-16 09:00:50 +0100

    Temporary translation

--- data.php
+++ data.php
@@ -4,45 +4,46 @@
 $svContent = [
   'headerContent' =>
   [
-    'title' => 'Be special - på svenska',
+    'title' => 'Var speciell',
+    'scroll' => 'Scrolla ner'
   ],
 
   'secondContent' =>
   [
-    'title' => 'Only with FS1.0',
+    'title' => 'Bara med FS1.0',
   ],
 
   'FS1.0'=>
   [
     'title' => 'FS1.0',
     'content' => 'Many people still think Saab makes cars, and that is correct. We are on the go for the future. Saab is very much alive and well, as one of the world’s most innovative companies. And now we are proud to present our car for the future: FastSecurity, FS.',
-    'button' => 'Sign up to the newsletter',
+    'button' => 'Anmäl dig till vårt nyhetsbrev',
   ],
 
   'performance'=>
   [
-    'title' => 'Performance på svenska',
+    'title' => 'Prestanda',
     'content' => 'With the newly developed Saab fusion-3 driveline, FS1.0 will be one of the fastest motorcycles in the world.',
-    'button' => 'Read more',
+    'button' => 'Läs mer',
   ],
 
   'exclusivity'=>
   [
-    'title' => 'Exclusivity på svenska',
+    'title' => 'Exklusivitet',
     'content' => 'With a production of only 500, FS1.0 will be one of the most exclusive and rare motorcycles in the world.',
-    'button' => 'Read more',
+    'button' => 'Läs mer',
   ],
 
   'merchandise'=>
   [
     'title' => 'Merchandise på svenska',
     'content' => 'Many people still think Saab makes cars, and that is correct. We are on the go for the future. Saab is very much alive and well.',
-    'button' => 'Read more',
+    'button' => 'Läs mer',
   ],
 
   'newsletter'=>
   [
-    'title' => 'Newsletter på svenska',
+    'title' => 'Nyhetsbrev',
     'content' => 'Sign up to our newsletter and getnews about the project as well as acess to our catalogue with exclusive content.',
   ],
 
@@ -64,7 +65,8 @@ $svContent = [
     'contact' => 'Kontakt',
     'phone' => 'Tel: +46 13 18 00 00',
     'visitingAdress' => 'Besöksadress: Olof Palmes gata 17, 5 tr, 111 22 Stockholm, Sweden',
-    'email' => 'Mail: info@saabgroup.com'
+    'email' => 'Mail: info@saabgroup.com',
+    'back' => 'Upp'
   ],
 ];
 
@@ -74,6 +76,7 @@ $enContent = [
   'headerContent' =>
   [
     'title' => 'Be special',
+    'scroll' => 'Scroll Down'
   ],
 
   'secondContent' =>
@@ -133,6 +136,7 @@ $enContent = [
     'contact' => 'Contact',
     'phone' => 'Phone: +44 (0)203 005 0480',
     'visitingAdress' => 'Visiting adress: 3rd Floor, 2 Savoy Court, Strand, London, WC2R 0EZ',
-    'email' => 'E-mail: saabtechnologiesuk@saabgroup.com'
+    'email' => 'E-mail: saabtechnologiesuk@saabgroup.com',
+    'back' => 'Back to top'
   ],
 ];
