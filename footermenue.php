59e33d38fc69195c112836c1711a47f97292044f
Erik Arvidsson <e@Es-MacBook-Pro.local>
2018-11-21 09:26:05 +0100

    added footermenue.php

---
+++ footermenue.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+<a href="#top"><p class="to-top"> <?=trans('companyContact','back')?> ⇧</p></a>
+<!-- <p class="visit"><?= trans(['companyContact']['backToTop'])?></p> -->
+
+<div>
+    <a href=""><?=trans('home','title')?></a>
+    <a href=""><?=trans('performance','title')?></a>
+    <a href=""><?=trans('exclusivity','title')?></a>
+    <a href=""><?=trans('merchandise','title')?></a>
+    <a href=""><?=trans('companyContact','contact')?></a>
+</div>
--- index.php
+++ index.php
@@ -48,9 +48,10 @@ if (isset($_GET['page'])) {
     require __DIR__."/main-content.php";
     require __DIR__."/signup.php";
     require __DIR__."/footer.php";
+    require __DIR__."/footermenue.php";
     set_url("index.php?lang=$lang&page=$page");
   }
-  
+
 
 
 
