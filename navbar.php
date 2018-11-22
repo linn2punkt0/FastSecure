52649dc916b77f512ea39cd8541f237a568579ce
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-11-22 11:38:54 +0100

    added logo to navbar

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 
 <nav>
     <div class="extra"></div>
-    <img src="" alt="Logo">
+    <img src="./FS_logo/SVG/logo_vit.svg" alt="Logo">
     <form action="index.php" method="get">
       <button type="submit" name="page" value="menu"><img src="" alt="burger"></button>
     </form>
