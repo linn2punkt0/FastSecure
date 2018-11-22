57b692293327ad300bff536f26f87f07b8179758
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-11-22 11:42:42 +0100

    added div to nav

--- navbar.php
+++ navbar.php
@@ -1,8 +1,9 @@
-
-<nav>
+<div class="menu">
+  <nav>
     <div class="extra"></div>
     <img src="./FS_logo/SVG/logo_vit.svg" alt="Logo">
     <form action="index.php" method="get">
       <button type="submit" name="page" value="menu"><img src="" alt="burger"></button>
     </form>
   </nav>
+</div>
