f0593bcdf619d5a729309886fe0f3dae143f476e
Isabelle Krall <isabellekrall@gmail.com>
2018-11-26 12:30:49 +0100

    cleaned up the code

--- footer.php
+++ footer.php
@@ -4,22 +4,13 @@
 
 <footer>
 
-<div class="fade-in">
-  <section class="content">
-    <ul>
-      <li>
-        <div class="right">
+
           <div class="contact">
             <h2 class="contactinfoh2"> <?= trans('companyContact', 'contact'); ?> </h2>
             <p class="contactinfo"><?= trans('companyContact', 'email')?></p>
             <p class="contactinfo"><?= trans('companyContact', 'phone')?></p>
             <p class="visit"><?= trans('companyContact', 'visitingAdress')?></p>
           </div>
-        </div>
-      </ul>
-    </li>
-</section>
-</div>
 
     <div class="footermenue">
 
@@ -65,7 +56,6 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="script.js"></script>
 
 </body>
 </html>
